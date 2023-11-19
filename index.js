67806f0b67a2a0a11744f4a0146af63f7f3045f4
Adivise <61177761+Adivise@users.noreply.github.com>
2023-11-19 15:44:29 +0700

    add config for edit host, port

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ process.on('unhandledRejection', error => console.log(error));
 process.on('uncaughtException', error => console.log(error));
 
 const client = new tmi.client(require("./config.json").twitch);
+const { host, port, socket } = require("./config.json");
 
 client.connect().then(() => {
   console.log("[INFO] Connected to Twitch!");
@@ -16,10 +17,10 @@ client.connect().then(() => {
       const command = args.shift().toLowerCase();
 
       if (command == "np" || command == "nowplaying") {
-        const response = await fetch(`http://127.0.0.1:24050/playing`).then(res => res.json());
+        const response = await fetch(`${host}:${port}/${socket}`).then(res => res.json());
         client.say(channel, `@${tags.username}, Beatmap: https://osu.ppy.sh/beatmapsets/${response.menu.bm.set}#/${response.menu.bm.id} | Mods: ${response.menu.mods.str}`);
       } else if (command == "skin") {
-        const response = await fetch(`http://127.0.0.1:24050/playing`).then(res => res.json());
+        const response = await fetch(`${host}:${port}/${socket}`).then(res => res.json());
         client.say(channel, `@${tags.username}, Skin: ${response.settings.folders.skin}`);
       }
   });
